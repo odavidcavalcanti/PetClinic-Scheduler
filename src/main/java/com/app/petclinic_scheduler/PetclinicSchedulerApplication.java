241083877563c904c59c1e70825b6f095b156ea8
odavidcavalcanti <davidomcavalcanti02@gmail.com>
2025-10-29 18:36:06 -0300

    initial: SpringBoot project initialized

---
+++ src/main/java/com/app/petclinic_scheduler/PetclinicSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.app.petclinic_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetclinicSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/petclinic_scheduler/PetclinicSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.petclinic_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
